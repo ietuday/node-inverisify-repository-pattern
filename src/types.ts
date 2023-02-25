371a255b55de4b876e3fcd9abce9f8a8b651a8df
Udayaditya Singh <udayaditya.singh@gmail.com>
2023-02-25 14:21:54 +0530

    fix

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+/**
+ * InversifyJS need to use the type as identifiers at runtime.
+ * We use symbols as identifiers but you can also use classes and or string literals.
+ */
+export const TYPES = {
+    UserService: Symbol('UserService'),
+    UserController: Symbol('UserController'),
+    UserRepository: Symbol('UserRepository'),
+    TaskService: Symbol('TaskService'),
+    TaskController: Symbol('TaskController'),
+    TaskRepository: Symbol('TaskRepository'),
+  };
+  
